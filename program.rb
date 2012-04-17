96a8cf54d9b1bd960a0e79e9a29a45b0b306e1c2
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-17 11:38:25 +0200

    Add Program

--- program.rb
+++ program.rb
@@ -1,4 +1,21 @@
 module Dcpu16Asm
   class Program < Treetop::Runtime::SyntaxNode
+    @@labels = Hash.new 
+    @@position = 0
+
+    def self.register_label(label)
+      raise "The label #{label} has already been defined!" if @@labels.key? label 
+
+      @@labels.store(label, @@position)
+    end
+
+    def compile(filename)
+      File.open(filename, 'wb') do |f|
+        elements.each do |instruction|
+          f.write instruction.to_bin
+          @@position += instruction.size
+        end
+      end
+    end
   end
 end
