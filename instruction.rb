3da9bcff9714c689c1b01066cc3fb2a60a4c4265
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-17 11:38:02 +0200

    Add methods to Instruction to output binary

--- instruction.rb
+++ instruction.rb
@@ -1,9 +1,29 @@
 module Dcpu16Asm
-
   class Instruction < Treetop::Runtime::SyntaxNode
     def to_bin
-      ["0000"].pack("b*")
+      # If there is a label, we need to register it
+      label.register if has_label?
+
+      # Find the instruction, either BasicInstruction or NonBasicInstruction
+      instruction.to_bin
+    end
+
+    def size
+      2
+    end
+
+    protected
+
+    def has_label?
+      not label.nil?
+    end
+
+    def label
+      @label ||= elements.select { |e| e.is_a? LabelDeclaration }.first
+    end
+
+    def instruction
+      @instruction ||= elements.select { |i| i.is_a?(BasicInstruction) || i.is_a?(NonBasicInstruction)}.first
     end
   end
-
 end
