af933d63761f40b6ebf89ef92ff93d73d3edb07f
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-17 11:37:00 +0200

    Use Program node to compile source.

--- dcpu16-ruby-asm.rb
+++ dcpu16-ruby-asm.rb
@@ -1,20 +1,30 @@
 require 'treetop'
+
+require_relative './register'
+require_relative './value'
+require_relative './basic_instruction_type'
+require_relative './basic_instruction'
+require_relative './non_basic_instruction'
+require_relative './label'
+require_relative './label_declaration'
 require_relative './instruction'
 require_relative './program'
 require_relative './dcpu16_grammar'
 
+raise "Need a source!" if ARGV.first.nil?
+dest = ARGV.first
+
 Treetop.load "dcpu16_grammar"
 
-program = <<ASM
+source = <<ASM
   SET A, 0x30              ; 7c01 0030
   SET [0x1000], 0x20       ; 7de1 1000 0020
-  SUB A, [0x1000]          ; 7803 1000
+  loop: SUB A, [0x1000]          ; 7803 1000
 ASM
 
 parser = Dcpu16AsmParser.new
-if result = parser.parse(program)
-    puts 'success'
-    p result.elements.size
+if program = parser.parse(source)
+  program.compile(dest)
 else
     puts parser.failure_reason
 end
