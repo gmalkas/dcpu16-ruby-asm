ad44f9bfc4c19f701e36b9f3ccc3d04c61a9d7b5
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-19 20:55:35 +0200

    Move files to dcpu16 folder

--- dcpu16-ruby-asm.rb
+++ dcpu16-ruby-asm.rb
@@ -1,24 +1,24 @@
+#! /usr/bin/env ruby
+
 require 'treetop'
 
-require_relative './register'
-require_relative './literal'
-require_relative './stack'
-require_relative './memory_value'
-require_relative './flag'
+require_relative './dcpu16/register'
+require_relative './dcpu16/literal'
+require_relative './dcpu16/stack'
+require_relative './dcpu16/memory_value'
+require_relative './dcpu16/flag'
 
-require_relative './basic_instruction_type'
-require_relative './basic_instruction'
-require_relative './non_basic_instruction'
-require_relative './non_basic_instruction_type'
+require_relative './dcpu16/basic_instruction_type'
+require_relative './dcpu16/basic_instruction'
+require_relative './dcpu16/non_basic_instruction'
+require_relative './dcpu16/non_basic_instruction_type'
 
-require_relative './label'
-require_relative './label_declaration'
+require_relative './dcpu16/label'
+require_relative './dcpu16/label_declaration'
 
-require_relative './instruction'
-require_relative './codeline'
-require_relative './program'
-
-require_relative './dcpu16_grammar'
+require_relative './dcpu16/instruction'
+require_relative './dcpu16/codeline'
+require_relative './dcpu16/program'
 
 if ARGV.size != 2
   puts "Format: dcpu16-ruby-asm source ouput" 
@@ -29,7 +29,8 @@ source_file, output_file = *ARGV
 
 source = File.read(source_file)
 
-Treetop.load "dcpu16_grammar"
+grammar_file = File.join(File.dirname(__FILE__), "dcpu16_grammar")
+Treetop.load grammar_file
 
 parser = Dcpu16AsmParser.new
 if program = parser.parse(source)
